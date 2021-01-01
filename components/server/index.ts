36e563ff162b3c3257220defd54d9fc3c63bd9ae
Albert Dugba <albert.dugba@gmail.com>
2021-01-01 01:51:31 +0000

    added a server

--- components/server/index.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import cors from "cors";
-import bodyParser from "body-parser";
-
-const app = express();
-
-app.use(cors);
-app.use(bodyParser.json());
---
+++ server/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(cors);
+app.use(bodyParser.json());
+
+const port = 4000;
+app.get("/posts", (_, res) => {
+  return res.json("posts");
+});
+app.get("/categories", (_, res) => {
+  return res.json("categories");
+});
+app.listen(port, () =>
+  console.log(`DB is running on http://localhost:${port}!`)
+);
